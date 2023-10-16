a7088f2febfaf2ede7491465c14087c84e842015
Sebastian W <sebastianmikael2000@gmail.com>
2023-10-16 11:48:14 +0200

    Initial push

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Import the CSV file
+    csv_file = "Occupancy_Estimation.csv"
+    df = pd.read_csv(csv_file)
+
+    # Print the DataFrame
+    print(df)
+
+
+
+
+
